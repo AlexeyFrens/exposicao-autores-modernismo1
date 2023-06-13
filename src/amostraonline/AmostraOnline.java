8909c35621840906633d46a0642eb87e0954e3f6
João Cláudio <leiloka46@gmail.com>
2023-06-12 22:12:25 -0300

    Adicionado projeto da exposição de autores

---
+++ src/amostraonline/AmostraOnline.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package amostraonline;
+
+import amostraonline.frames.LoginFrame;
+
+/**
+ *
+ * @author João Cláudio
+ */
+public class AmostraOnline {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LoginFrame().setVisible(true);
+        
+        //Cria uma nova janela e permite que o usuário consiga vê-la
+        
+    }
+    
+}
